047ef90a9fc0683332c2f703576712514c8f1d59
Alchemystx <wierdalchemist@gmail.com>
2026-02-09 19:49:03 +0100

    config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ const CONFIG = {
     questions: {
         first: {
             text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
+            yesBtn: "A Lot",                                             // Text for "Yes" button
+            noBtn: "A Little Bit",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
